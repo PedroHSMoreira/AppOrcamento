4469ceca39200bc1cbe984969ae6bb117e24c784
Pedro Henrique Moreira <pedro@tecnologiabasica.com.br>
2019-08-09 23:50:27 -0300

    MOD.: search expenses

--- app.js
+++ app.js
@@ -6,78 +6,111 @@ class Expense {
       (this.day = day),
       (this.type = type),
       (this.description = description),
-      (this.val = val);
+      (this.val = val)
   }
 
   validator() {
     for (const i in this) {
-      if (this[i] == undefined || this[i] == "" || this[i] == null) {
-        return false;
+      if (this[i] == undefined || this[i] == '' || this[i] == null) {
+        return false
       }
     }
-    return true;
+    return true
   }
 }
 
 // Classe que simula um banco de dados com localStorage
 class DB {
   constructor() {
-    let id = localStorage.getItem("id");
+    let id = localStorage.getItem('id')
 
     if (id === null) {
-      localStorage.setItem("id", 0);
+      localStorage.setItem('id', 0)
     }
   }
 
   getId() {
     // Gerencia o id das despesas
-    let nextId = localStorage.getItem("id");
-    return parseInt(nextId) + 1;
+    let nextId = localStorage.getItem('id')
+    return parseInt(nextId) + 1
   }
 
   setStorage(expense) {
     // Armazena a despesa em localStorage
-    let id = this.getId();
+    let id = this.getId()
 
-    localStorage.setItem(`${id}`, JSON.stringify(expense)); // Transforma o objeto literal em um json string
+    localStorage.setItem(`${id}`, JSON.stringify(expense)) // Transforma o objeto literal em um json string
 
-    localStorage.setItem("id", id);
+    localStorage.setItem('id', id)
   }
 
   getAllExpense() {
-    let expenses = [];
+    let expenses = []
 
-    let id = localStorage.getItem("id");
+    let id = localStorage.getItem('id')
 
     for (let i = 1; i <= id; i++) {
       // Busca as despesas por id
-      let expense = JSON.parse(localStorage.getItem(i)); // Transforma um json em objeto literal
+      let expense = JSON.parse(localStorage.getItem(i)) // Transforma um json em objeto literal
       if (expense === null) {
-        continue;
+        continue
       }
-      expenses.push(expense); //Adiciona todas as despesas no array
+      expenses.push(expense) //Adiciona todas as despesas no array
     }
-    return expenses;
+    return expenses
   }
 
-  search() {
-      
+  search(expense) {
+    let filtredExpenses = []
+
+    filtredExpenses = this.getAllExpense()
+    console.log(expense)
+    console.log(filtredExpenses)
+    // ano
+    if (expense.year != '') {
+      filtredExpenses = filtredExpenses.filter(el => el.year == expense.year)
+      console.log(filtredExpenses)
+    }
+    // Mês
+    if (expense.month != '') {
+      filtredExpenses = filtredExpenses.filter(el => el.month == expense.month)
+    }
+    // Dia
+    if (expense.day != '') {
+      filtredExpenses = filtredExpenses.filter(el => el.day == expense.day)
+    }
+    // Tipo
+    if (expense.type != '') {
+      filtredExpenses = filtredExpenses.filter(el => el.type == expense.type)
+    }
+    // Descrição
+    if (expense.description != '') {
+      filtredExpenses = filtredExpenses.filter(
+        el => el.description == expense.description
+      )
+    }
+    // Valor
+    if (expense.val != '') {
+      filtredExpenses = filtredExpenses.filter(el => el.val == expense.val)
+    }
+
+    return filtredExpenses
   }
 }
 /*------------------------------ 
     Controllers da aplicação 
 */
 
-let db = new DB();
+let db = new DB()
 
 // Cadastrando nova despesa
 function registerExpense() {
-  let year = document.querySelector("#year"); // Acessando os elementos do formulário
-  let month = document.querySelector("#month");
-  let day = document.querySelector("#day");
-  let type = document.querySelector("#type");
-  let description = document.querySelector("#description");
-  let val = document.querySelector("#val");
+  let year = document.querySelector('#year') // Acessando os elementos do formulário
+  let month = document.querySelector('#month')
+  let day = document.querySelector('#day')
+  let type = document.querySelector('#type')
+  let description = document.querySelector('#description')
+  let val = document.querySelector('#val')
 
   let expense = new Expense(
     year.value,
@@ -86,81 +119,104 @@ function registerExpense() {
     type.value,
     description.value,
     val.value
-  );
+  )
 
   if (expense.validator()) {
-    db.setStorage(expense);
-    messageAlert("success", "Despesa cadastrada com sucesso!");
-    console.log("Válido");
+    db.setStorage(expense)
+    messageAlert('success', 'Despesa cadastrada com sucesso!')
+    console.log('Válido')
     // Limpando os campos do formulário
-    year.value = "";
-    month.value = "";
-    day.value = "";
-    type.value = "";
-    description.value = "";
-    val.value = "";
+    year.value = ''
+    month.value = ''
+    day.value = ''
+    type.value = ''
+    description.value = ''
+    val.value = ''
   } else {
-    console.log("Inválido");
+    console.log('Inválido')
     messageAlert(
-      "danger",
-      "Não foi possível cadastrar sua despesa! Verifique se os campos foram preenchidos!"
-    );
+      'danger',
+      'Não foi possível cadastrar sua despesa! Verifique se os campos foram preenchidos!'
+    )
   }
 }
 
 function messageAlert(type, message) {
   // Alert
-  let alert = document.querySelector("#messageAlert");
+  let alert = document.querySelector('#messageAlert')
 
-  if (alert.classList.contains("fade")) {
+  if (alert.classList.contains('fade')) {
     // Verificando se o alert está em modo fade(escondido)
-    alert.classList.remove("fade");
+    alert.classList.remove('fade')
     setTimeout(() => {
-      alert.classList.add("fade");
-      alert.classList.remove(`alert-${type}`);
-    }, 3000); // Visível por 3s
+      alert.classList.add('fade')
+      alert.classList.remove(`alert-${type}`)
+    }, 3000) // Visível por 3s
   }
 
-  alert.classList.add(`alert-${type}`);
-  alert.textContent = message;
+  alert.classList.add(`alert-${type}`)
+  alert.textContent = message
 }
 
-function getList() {
-  let expenses = [];
+function getList(expense = [], filtred = false) {
+  let tableExpenses = document.querySelector('#tableExpenses')
+  tableExpenses.innerHTML = '' // Limapando a tabela
 
-  expenses = db.getAllExpense();
+  if (expense.length == 0 && filtred === false) {
+    expense = db.getAllExpense()
+  } 
+  else if (expense.length == 0 && filtred == true) {
+    messageAlert('danger', 'Nenhuma despesa encontrada!')
+  }  
 
-  let tableExpenses = document.querySelector("#tableExpenses");
-
-  expenses.forEach(el => {
-    let row = tableExpenses.insertRow(); // Insere linhas <tr> no body da tabela
+  expense.forEach(el => {
+    let row = tableExpenses.insertRow() // Insere linhas <tr> no body da tabela
 
     // Insere colunas <td> nas linhas
-    row.insertCell(0).innerHTML = `${el.day}/${el.month}/${el.year}`;
+    row.insertCell(0).innerHTML = `${el.day}/${el.month}/${el.year}`
     // Tratativa do campo tipo
     switch (el.type) {
-      case "1":
-        el.type = "Alimentação";
-        break;
-      case "2":
-        el.type = "Educação";
-        break;
-      case "3":
-        el.type = "Lazer";
-        break;
-      case "4":
-        el.type = "Saúde";
-        break;
-      case "5":
-        el.type = "Transporte";
-        break;
+      case '1':
+        el.type = 'Alimentação'
+        break
+      case '2':
+        el.type = 'Educação'
+        break
+      case '3':
+        el.type = 'Lazer'
+        break
+      case '4':
+        el.type = 'Saúde'
+        break
+      case '5':
+        el.type = 'Transporte'
+        break
     }
-    row.insertCell(1).innerHTML = el.type;
-    row.insertCell(2).innerHTML = el.description;
-    row.insertCell(3).innerHTML = el.val;
-  });
+    row.insertCell(1).innerHTML = el.type
+    row.insertCell(2).innerHTML = el.description
+    row.insertCell(3).innerHTML = el.val
+  })
 }
 
 function searchExpense() {
+  let year = document.querySelector('#year') // Acessando os elementos do formulário
+  let month = document.querySelector('#month')
+  let day = document.querySelector('#day')
+  let type = document.querySelector('#type')
+  let description = document.querySelector('#description')
+  let val = document.querySelector('#val')
 
+  let expense = new Expense(
+    year.value,
+    month.value,
+    day.value,
+    type.value,
+    description.value,
+    val.value
+  )
+
+  let filtredExpenses = db.search(expense)
+
+  getList(filtredExpenses, true)
+  
 }
